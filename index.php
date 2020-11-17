67cd1b6b3fffab51bcf292147a159feedebc9b92
mjeda <abdulrazak.amd@gmail.com>
2020-11-17 02:05:42 -0800

    change made with sign in and sign up view

--- index.php
+++ index.php
@@ -12,42 +12,43 @@
 <body class="bg-light ">
 <div class="container px-5 py-2">
     <div class="container row">
-    <div class="col-12 col-sm-2 col-md-3 col-lg-3"></div>
-    <div class=" card col-12 col-sm-8 col-md-6 col-lg-6 my-5">
-      <div class="card-title text-center h2 p-3">
-        STUDENT INTERNSHIP
-      </div>
-      <div class="card-title text-center h2 p-3">
-        Log in
-      </div>
-      <?php 
-        session_start();
-        if(isset($_SESSION['error_msg'])){
-          ?><span class="text-center text-danger"><?php print($_SESSION['error_msg']); ?><span><?php
-          session_unset();
-        }
-      ?>
-      <form action="pages/index_handler.php" method="POST" class="pb-3">
-      <div class="col-12 pt-3 pr-3 pl-3">
-          <input type="text" class="form-control" name="Email" id="email" placeholder="Email" required>
-        </div>
-        <div class="col-12 pt-3 pr-3 pl-3">
-          <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-        </div>
-        <div class="row">
-          <div class="col-4"></div>
-          <div class="col-4  pt-3 pr-4 pl-4">
-            <button type="submit" class="btn btn-primary btn-block" name="login">Log in</button>
+      <div class="col-12 col-sm-2 col-md-3 col-lg-3"></div>
+      <div class="col-12 col-sm-8 col-md-6">
+        <div class="card my-5 shadow ">
+          <div class="card-header card-title text-center bg-primary">
+            <h5 class="text-white text-uppercase"> Intern & field system</h5>
+          </div>
+          <?php 
+            session_start();
+            if(isset($_SESSION['error_msg'])){
+              ?><span class="text-center text-danger"><?php print($_SESSION['error_msg']); ?><span><?php
+              session_unset();
+            }
+          ?>
+          <form action="pages/index_handler.php" method="POST" class="pb-3 shadow">
+          <div class="col-12 pt-3 pr-3 pl-3">
+              <input type="text" class="form-control" name="Email" id="email" placeholder="Email" required>
+            </div>
+            <div class="col-12 pt-3 pr-3 pl-3">
+              <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+            </div>
+            <div class="row">
+              <div class="col-2"></div>
+              <div class="col-8  pt-3 pr-4 pl-4">
+                <button type="submit" class="btn btn-primary btn-block" name="login">Sign in</button>
+              </div>
+            </div>
+          </form>
+          <div class="text-center small">
+            <p>
+              <a href="#" class="small">Forget Password</a>
+            </p>
+            <p>You have not account?
+              <a href="pages/register_form.php" class="small">Sign up</a>
+            </p>
           </div>
         </div>
-      </form>
-      <p class="text-center">
-        <a href="#" class="small">Forget Password</a>
-      </p>
-      <p class="text-center">
-        <a href="pages/register_form.php" class="small">Create Account</a>
-      </p>
-    </div>
+      </div>
     </div>
   </div>
 <script src="asset/jquery/jquery.min.js"></script>
--- pages/register_form.php
+++ pages/register_form.php
@@ -13,47 +13,51 @@
 <div class="container px-5 py-2">
     <div class="container row">
     <div class="col-12 col-sm-2 col-md-3 col-lg-3"></div>
-    <div class=" card col-12 col-sm-8 col-md-6 col-lg-6 my-5">
-    
-      <div class="card-title text-center h2 p-3">
-        <span class="border-bottom"></span>
-        <span>STUDENT INTERSHIP REGISTER </span>
-      </div>
-      <form action="register_handler.php" method="POST" class="pb-3">
-      <div class="row pt-3 pr-3 pl-3">
-          <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-6">
+      <div class="card my-5 shadow">
+        <div class="card-header card-title text-center bg-primary">
+          <h5 class="text-white text-uppercase"> Intern & field system</h5>
         </div>
-        <div class="row pt-3 pr-3 pl-3">
-          <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
-          <?php 
-            session_start();
-            if(isset($_SESSION['e_msg'])){
-              ?><span class="text-center text-danger"><?php print($_SESSION['e_msg']); ?><span><?php
-              session_unset();
-            }
-          ?>
-        </div>
-        <div class="row p-3">
-          <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-        </div>
-        <div class="row pb-3 pr-3 pl-3">
-          <input type="password" class="form-control" name="conf_password" id="conf_password" placeholder="Confirm Password">
-          <?php
-            if(isset($_SESSION['p_msg'])){
-              ?><span class="text-center text-danger"><?php print($_SESSION['p_msg']); ?><span><?php
-              session_unset();
-            }
-          ?>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <button type="submit" name="register" class="btn btn-primary btn-block">Register</button>
+        <form action="register_handler.php" method="POST" class="pb-3 shadow">
+          <div class="col-12 pt-3 pr-3 pl-3">
+              <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
           </div>
-        </div>
-      </form>
-      <p class="text-center small">If Registered
-        <a href="../index.php">log-in</a>
-      </p>
+          <div class="col-12 pt-3 pr-3 pl-3">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+            <?php 
+              session_start();
+              if(isset($_SESSION['e_msg'])){
+                ?><span class="text-center text-danger"><?php print($_SESSION['e_msg']); ?><span><?php
+                session_unset();
+              }
+            ?>
+          </div>
+          <div class="col-12 p-3">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+          </div>
+          <div class="col-12 pb-3 pr-3 pl-3">
+            <input type="password" class="form-control" name="conf_password" id="conf_password" placeholder="Confirm Password">
+            <?php
+              if(isset($_SESSION['p_msg'])){
+                ?><span class="text-center text-danger"><?php print($_SESSION['p_msg']); ?><span><?php
+                session_unset();
+              }
+            ?>
+          </div>
+          <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8  pt-3 pr-4 pl-4">
+              <button type="submit" class="btn btn-primary btn-block" name="login">Sign up</button>
+            </div>
+          </div>
+        </form>
+      <div class="text-center small">
+        <br>
+        <p>If you have account
+          <a href="../index.php">Sign in</a>
+        </p>
+      </div>
+    </div>
     </div>
     </div>
   </div>
